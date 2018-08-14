1d38a543a2515241df834514ac581173386d7f92
Yash Pande <t-yapand@microsoft.com>
2018-08-14 11:00:46 -0700

    Updated to latest code

--- run_detection.py
+++
@@ -1,6 +0,0 @@
-from detect_object import detect
-
-input_dir = 'C:\\data\\woodknots\\board_images_set2'
-output_dir = 'C:\\data\\woodknots\\set2_res'
-model_name = 'bla'
-detect(input_dir, output_dir, model_name)
--- run_process_images.py
+++
@@ -1,4 +0,0 @@
-from process_images import crop_objects
-input = r'C:\data\woodknots\board_images_set2'
-output =  r'C:\data\woodknots\set2_res'
-crop_objects(input, output)
