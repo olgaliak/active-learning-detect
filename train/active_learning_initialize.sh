c1f027185f5c85ef21e0f224cd000dfb7c7bf033
olgaliak <olgali@outlook.com>
2018-08-14 16:22:58 -0700

    fix for config unwrapping in sh and python

--- train/active_learning_initialize.sh
+++ train/active_learning_initialize.sh
@@ -7,7 +7,8 @@ set +a
 # Make all necessary directories
 mkdir -p $image_dir
 # Download all images
-az storage blob download-batch --source $image_container_name --destination $image_dir
+#az storage blob download-batch --source $image_container_name --destination $image_dir
 # Create TFRecord from images + csv file on blob storage
 # TODO: Try to import create_predictions into this
-python ${python_file_directory}/initialize_vott_pull.py "$@"
+envsubst < $1 > cur_config.ini
+python ${python_file_directory}/initialize_vott_pull.py cur_config.ini 
--- train/active_learning_train.sh
+++ train/active_learning_train.sh
@@ -4,12 +4,14 @@ set -a
 sed -i 's/\r//g' $1
 . $1
 set +a
+# Updating vars in config file
+envsubst < $1 > cur_config.ini
 # Update images from blob storage
 echo "Updating Blob Folder"
-python ${python_file_directory}/update_blob_folder.py "$@"
+python ${python_file_directory}/update_blob_folder.py cur_config.ini
 # Create TFRecord from images + csv file on blob storage
 echo "Creating TF Record"
-python ${python_file_directory}/convert_tf_record.py "$@"
+python ${python_file_directory}/convert_tf_record.py cur_config.ini
 # pipeline.config from bash variables
 echo "Making pipeline file from env vars"
 temp_pipeline=${pipeline_file%.*}_temp.${pipeline_file##*.}
@@ -25,9 +27,9 @@ python ${tf_location}/export_inference_graph.py --input_type image_tensor --pipe
 # TODO: Validation on Model, keep track of MAP etc.
 # Use inference graph to create predictions on untagged images
 echo "Creating new predictions"
-python ${python_file_directory}/create_predictions.py "$@"
+python ${python_file_directory}/create_predictions.py cur_config.ini
 echo "Calculating performance"
-python ${python_file_directory}/validation.py "$@"
+python ${python_file_directory}/validation.py cur_config.ini
 # Rename predictions and inference graph based on timestamp and upload
 echo "Uploading new data"
 az storage blob upload --container-name activelearninglabels --file ${inference_output_dir}/frozen_inference_graph.pb --name model_$(date +%s).pb  --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
