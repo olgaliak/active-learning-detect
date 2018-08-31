82e14d43eae57c18b1f52ebfeb2cb798850dbae3
Ubuntu <yashpande@yashpandeubundudsvm.hy0shrllycbu5bcd1njhod0jye.jx.internal.cloudapp.net>
2018-08-31 03:42:39 +0000

    Updated decoder

--- utils/decode_tf_record.py
+++ utils/decode_tf_record.py
@@ -1,58 +1,36 @@
 import numpy as np
 import tensorflow as tf
 from pathlib import Path
-def decode_record(record_file, split_names=["train","val"], split_percent=[.7,.3]):
-    record_file = Path(record_file)
+import cv2
+def decode_record(record_file, output_folder):
+    output_folder = Path(output_folder)
+    output_folder.mkdir(parents=True, exist_ok=True)
+    record_iterator = tf.python_io.tf_record_iterator(record_file)
+    for string_record in record_iterator:
+        example = tf.train.Example()
+        example.ParseFromString(string_record)
+            
+        filename = example.features.feature['image/filename']
+                                   .bytes_list
+                                   .value[0].decode("utf-8")
 
-    for name in split_names:
-
-        reconstructed_images = []
-
-        record_iterator = tf.python_io.tf_record_iterator("{}_{}".format(record_file.with_suffix(''), name) + record_file.suffix)
-
-        for string_record in record_iterator:
-            
-            example = tf.train.Example()
-            example.ParseFromString(string_record)
-            
-            height = int(example.features.feature['image/height']
-                                        .int64_list
-                                        .value[0])
-            
-            width = int(example.features.feature['image/width']
-                                        .int64_list
-                                        .value[0])
-            
-            img_string = (example.features.feature['image/encoded']
-                                        .bytes_list
-                                        .value[0])
-
-            # img_string = (example.features.feature['image_raw']
-            #                             .bytes_list
-            #                             .value[0])
-
-            # annotation_string = (example.features.feature['mask_raw']
-            #                             .bytes_list
-            #                             .value[0])
-            
-            img_1d = np.fromstring(img_string, dtype=np.uint8)
-            reconstructed_img = img_1d.reshape((height, width, -1))
-            
-            # annotation_1d = np.fromstring(annotation_string, dtype=np.uint8)
-            
-            # Annotations don't have depth (3rd dimension)
-            #reconstructed_annotation = annotation_1d.reshape((height, width))
-            
-            #reconstructed_images.append((reconstructed_img, reconstructed_annotation))
-            reconstructed_images.append(reconstructed_img)
-        print(len(reconstructed_images))
+        height = int(example.features.feature['image/height']
+                                    .int64_list
+                                    .value[0])
+          
+        width = int(example.features.feature['image/width']
+                                    .int64_list
+                                    .value[0])
+         
+        img_raw = (example.features.feature['image/encoded']
+                                    .bytes_list
+                                    .value[0])
+        img_raw = np.fromstring(img_raw, dtype=np.uint8)
+        cv2_image = cv2.imdecode(img_raw, cv2.IMREAD_COLOR)
+        cv2.imwrite(str(output_folder/filename),cv2_image)
 if __name__ == "__main__":
-    import sys
-    config_dir = str(Path.cwd().parent / "utils")
-    if config_dir not in sys.path:
-        sys.path.append(config_dir)
-    from config import Config
-    if len(sys.argv)<2:
-        raise ValueError("Need to specify config file")
-    config_file = Config.parse_file(sys.argv[1])
-    decode_record(config_file["tf_record_location"])
+    if len(sys.argv)<3:
+        raise ValueError("Need to specify input file and output folder")
+    input_file = sys.argv[1]
+    output_folder = sys.argv[2]
+    decode_record(input_file, output_folder)
