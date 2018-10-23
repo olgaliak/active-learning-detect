634aed8ddc4ce12d0270883269db9a8a98f56dfa
olgaliak <olgali@outlook.com>
2018-10-22 18:32:58 -0700

    download_vott_json refactoring: simplifing "user_folder" flow, adding more test

--- tag/download_vott_json.py
+++ tag/download_vott_json.py
@@ -8,6 +8,7 @@ import shutil
 import random
 import colorsys
 import numpy as np
+from io import StringIO
 
 CONFIDENCE_LOCATION = -1
 TAG_CONFIDENCE_LOCATION = -2
@@ -109,64 +110,90 @@ def make_vott_output(all_predictions, output_location, user_folders, image_loc, 
     with open(str(output_location)+".json","w") as json_out:
         json.dump(dirjson, json_out)
 
-def get_top_rows(file_location, num_rows, user_folders, pick_max, tag_names, ideal_class_balance):
-    if ideal_class_balance is not None and len(tag_names) != len(ideal_class_balance):
-        print("WARNING: Mismatch between number of classes:{} and ideal_class_balance: {}".format(tag_names,
-                                                                                                  ideal_class_balance))
-        print("Defaulting to ideal_class_balance: None")
-        ideal_class_balance = None
+
+def select_rows(arr_image_data, num_rows, is_largest):
+    if is_largest:
+        top = nlargest(num_rows, arr_image_data,
+                       key=lambda x: float(x[0][CONFIDENCE_LOCATION]))
+    else:
+        top = nsmallest(num_rows, arr_image_data,
+                       key=lambda x: float(x[0][CONFIDENCE_LOCATION]))
+
+    return top
+
+def prepare_per_class_dict(all_files_per_folder, class_balances_cnt, tag_names):
+    result = {}
+    for k, v in all_files_per_folder.items():
+        v_arr = np.array(v)
+        classes = v_arr[:, TAG_LOCATION]
+        for i in range(class_balances_cnt):
+            class_i = tag_names[i]
+            if class_i in classes:
+                if class_i not in result:
+                    result[class_i] = []
+                result[class_i].append(v)
+
+    return result
+
+
+def parse_class_balance_setting(config_value, expected_cnt):
+    if config_value is None:
+        return  None
+    arr_np = np.genfromtxt(StringIO(config_value), dtype=float, delimiter=',', loose=True)
+    # check f there were non valid numbers
+    if np.isnan(arr_np.any()):
+        print("Found NaNs in ideal balance settings:", config_value)
+        return None
+    else:
+        if (arr_np.size != expected_cnt):
+            print("Size of ideal balance settings {0} is {1}. Expected {2}".format(arr_np.size, arr_np, expected_cnt))
+            return None
+
+        s = np.sum(arr_np)
+        if s == 1:
+            return arr_np
+        else:
+            print("Sum of balance settings {0} should add up to 1: {1}".format(config_value, s) )
+
+
+def get_top_rows(file_location, num_rows, user_folders, pick_max, tag_names, config_class_balance):
+    ideal_class_balance = parse_class_balance_setting(config_class_balance, len(tag_names))
 
     with (file_location/"totag.csv").open(mode='r') as file:
         reader = csv.reader(file)
         header = next(reader)
         csv_list = list(reader)
-    all_files_per_class = {}
-    if user_folders:
-        all_files = defaultdict(lambda: defaultdict(list))
-        for row in csv_list:
-            all_files[row[FOLDER_LOCATION]][row[0]].append(row)
-        all_lists = []
-        class_balances_cnt = 1
+
+    all_files = defaultdict(lambda: defaultdict(list))
+    for row in csv_list:
+        all_files[row[FOLDER_LOCATION]][row[0]].append(row)
+    all_lists = []
+    class_balances_cnt = 1
+    if ideal_class_balance is not None:
+        class_balances_cnt = len(ideal_class_balance)
+
+
+    for folder_name in all_files:
         if ideal_class_balance is not None:
-            class_balances_cnt = len(ideal_class_balance)
-        if pick_max:
-            for folder_name in all_files:
-                for k, v in all_files[folder_name].items():
-                    v_arr = np.array(v)
-                    classes = v_arr[:, TAG_LOCATION]
-                    for i in range(class_balances_cnt):
-                        class_i = tag_names[i]
-                        if class_i in classes:
-                            if class_i not in all_files_per_class:
-                                #all_files_per_class[class_i] = {}
-                                all_files_per_class[class_i] = []
-                            #all_files_per_class[class_i][k] = v
-                            all_files_per_class[class_i].append(v)
-                            break;
+            all_files_per_class = prepare_per_class_dict(all_files[folder_name], class_balances_cnt, tag_names)
+            for i in range(class_balances_cnt):
+                num_rows_i = round(num_rows * float(ideal_class_balance[i]))
+                class_i = tag_names[i]
+                top = select_rows(all_files_per_class[class_i], num_rows_i, is_largest = pick_max)
 
+                # drop values we selected from the dict
+                # the same image may have object from diff classes
+                for j in range(class_balances_cnt):
+                    class_j = tag_names[j]
+                    all_files_per_class[class_j] = [v for  v in all_files_per_class[class_j]
+                                       if v not in top]
+                all_lists = all_lists + top
+        else:
+             top = select_rows(all_files[folder_name].values(), num_rows, is_largest = pick_max)
 
-                for i in range(class_balances_cnt):
-                    num_rows_i = int(num_rows * float(ideal_class_balance[i]))
-                    class_i = tag_names[i]
-                    top = nlargest(num_rows_i, all_files_per_class[class_i],
-                                              key=lambda x: float(x[0][CONFIDENCE_LOCATION]))
-                    all_lists = all_lists + top
-                #all_lists.append(nlargest(num_rows, all_files[folder_name].values(), key=lambda x:float(x[0][CONFIDENCE_LOCATION])))
-            #top_rows = max(all_lists,key=lambda x:sum(float(row[0][CONFIDENCE_LOCATION]) for row in x))
-            top_rows = all_lists
-        else:
-            for folder_name in all_files:
-                all_lists.append(nsmallest(num_rows, all_files[folder_name].values(), key=lambda x:float(x[0][CONFIDENCE_LOCATION])))
-            top_rows = min(all_lists,key=lambda x:sum(float(row[0][CONFIDENCE_LOCATION]) for row in x))
-    else:
-        all_files = defaultdict(list)
-        for row in csv_list:
-            all_files[row[0]].append(row)
-        if pick_max:
-            top_rows = nlargest(num_rows, all_files.values(), key=lambda x:float(x[0][CONFIDENCE_LOCATION]))
-        else:
-            top_rows = nsmallest(num_rows, all_files.values(), key=lambda x:float(x[0][CONFIDENCE_LOCATION]))
-    tagging_files = {row[0][0] for row in top_rows}
+             all_lists = all_lists + top
+
+    tagging_files = {row[0][0] for row in all_lists }
     file_exists = (file_location/"tagging.csv").is_file()
     with (file_location/"totag.csv").open(mode='w', newline='') as untagged, (file_location/"tagging.csv").open(mode='a', newline='') as tagging:
         untagged_writer, tagging_writer = csv.writer(untagged), csv.writer(tagging)
@@ -175,17 +202,17 @@ def get_top_rows(file_location, num_rows, user_folders, pick_max, tag_names, ide
             tagging_writer.writerow(header)
         for row in csv_list:
             (tagging_writer if row[0] in tagging_files else untagged_writer).writerow(row)
-    return top_rows
+    return all_lists
 
 def create_vott_json(file_location, num_rows, user_folders, pick_max, image_loc, output_location, blob_credentials=None,
-                     tag_names = ["stamp"], max_tags_per_pixel=None, ideal_class_balance=None):
-    all_files = get_top_rows(file_location, num_rows, user_folders, pick_max, tag_names, ideal_class_balance)
+                     tag_names = ["stamp"], max_tags_per_pixel=None, config_class_balance=None):
+    all_rows = get_top_rows(file_location, num_rows, user_folders, pick_max, tag_names, config_class_balance)
     # The tag_colors list generates random colors for each tag. To ensure that these colors stand out / are easy to see on a picture, the colors are generated
     # in the hls format, with the random numbers biased towards a high luminosity (>=.8) and saturation (>=.75).
     colors = ['#%02x%02x%02x' % (int(256*r), int(256*g), int(256*b)) for
             r,g,b in [colorsys.hls_to_rgb(random.random(),0.8 + random.random()/5.0, 0.75 + random.random()/4.0) for _ in tag_names]]
 
-    make_vott_output(all_files, output_location, user_folders, image_loc, blob_credentials=blob_credentials,
+    make_vott_output(all_rows, output_location, user_folders, image_loc, blob_credentials=blob_credentials,
                      tag_names=tag_names,  tag_colors=colors, max_tags_per_pixel=max_tags_per_pixel)
 
 if __name__ == "__main__":
@@ -219,7 +246,7 @@ if __name__ == "__main__":
                      config_file["tagging_location"], blob_credentials=(block_blob_service, container_name),
                      tag_names=config_file["classes"].split(","),
                      max_tags_per_pixel=config_file.get("max_tags_per_pixel"),
-                     ideal_class_balance =config_file.get("ideal_class_balance").split(","))
+                     config_class_balance =config_file.get("ideal_class_balance"))
     container_name = config_file["label_container_name"]
     block_blob_service.create_blob_from_path(container_name, "{}_{}.{}".format("tagging",int(time.time() * 1000),"csv"), str(csv_file_loc/"tagging.csv"))
     block_blob_service.create_blob_from_path(container_name, "{}_{}.{}".format("totag",int(time.time() * 1000),"csv"), str(csv_file_loc/"totag.csv"))
--- test/test_download_vott_json.py
+++ test/test_download_vott_json.py
@@ -27,7 +27,7 @@ class DownloadVOTTJSONTestCase(unittest.TestCase):
         shutil.copyfile("./totag_source.csv", str(self.csv_file_loc / "totag.csv"))
 
         self.csv_file_loc.mkdir(parents=True, exist_ok=True)
-        self.ideal_class_balance = self.config_file["ideal_class_balance"].split(",")
+        self.ideal_class_balance = self.config_file["ideal_class_balance"]
         self. max_tags_per_pixel = self.config_file.get("max_tags_per_pixel")
         self.tag_names = self.config_file["classes"].split(",")
         self.user_folders = self.config_file["user_folders"] == "True"
@@ -51,8 +51,79 @@ class DownloadVOTTJSONTestCase(unittest.TestCase):
         all_rows = get_top_rows(self.csv_file_loc, N_ROWS, self.user_folders ,
                          self.pick_max, self.tag_names, self.ideal_class_balance)
         self.assertEqual(len(all_rows), N_FILES, 'number of rows')
-        self.assertEqual(all_rows, EXPECTED, 'row values')
+        self.assertEqual(all_rows, EXPECTED, 'raw values')
 
+    #@unittest.skip
+    def test_get_top_rows_empty_class_balance(self):
+        N_ROWS = 3
+        N_FILES = 3
+        EXPECTED = [[['st1840.png', 'knot', '0.12036637', '0.18497443', '0.7618415', '0.8283344', '512', '488', 'board_images_png', '0.986', '0.986'],
+                     ['st1840.png', 'knot', '0.7297609', '0.7755673', '0.62443626', '0.6670296', '512', '488', 'board_images_png', '0.986', '0.986'],
+                     ['st1840.png', 'defect', '0.76513', '0.9952971', '0.6075407', '0.6546806', '512', '488', 'board_images_png', '0.986', '0.986']],
+                     [['st1578.png', 'knot', '0.594302', '0.6663906', '0.35276932', '0.43525606', '512', '488', 'board_images_png', '0.98448783', '0.98448783']],
+                     [['st1611.png', 'knot', '0.6326234', '0.7054164', '0.86741334', '0.96444726', '512', '488', 'board_images_png', '0.99616516', '0.9843567'],
+                      ['st1611.png', 'knot', '0.07399843', '0.11282173', '0.32572043', '0.36819047', '512', '488', 'board_images_png', '0.9843567', '0.9843567']]]
+        class_balance = ''
+        all_rows = get_top_rows(self.csv_file_loc, N_ROWS, self.user_folders ,
+                         self.pick_max, self.tag_names, class_balance)
+        self.assertEqual(len(all_rows), N_FILES, 'number of rows')
+        self.assertEqual(all_rows, EXPECTED, 'raw values')
+
+    def test_get_top_rows_invalid_class_balance1(self):
+        N_ROWS = 3
+        N_FILES = 3
+        EXPECTED = [[['st1840.png', 'knot', '0.12036637', '0.18497443', '0.7618415', '0.8283344', '512', '488', 'board_images_png', '0.986', '0.986'],
+                     ['st1840.png', 'knot', '0.7297609', '0.7755673', '0.62443626', '0.6670296', '512', '488', 'board_images_png', '0.986', '0.986'],
+                     ['st1840.png', 'defect', '0.76513', '0.9952971', '0.6075407', '0.6546806', '512', '488', 'board_images_png', '0.986', '0.986']],
+                     [['st1578.png', 'knot', '0.594302', '0.6663906', '0.35276932', '0.43525606', '512', '488', 'board_images_png', '0.98448783', '0.98448783']],
+                     [['st1611.png', 'knot', '0.6326234', '0.7054164', '0.86741334', '0.96444726', '512', '488', 'board_images_png', '0.99616516', '0.9843567'],
+                      ['st1611.png', 'knot', '0.07399843', '0.11282173', '0.32572043', '0.36819047', '512', '488', 'board_images_png', '0.9843567', '0.9843567']]]
+        class_balance = 'Random'
+        all_rows = get_top_rows(self.csv_file_loc, N_ROWS, self.user_folders ,
+                         self.pick_max, self.tag_names, class_balance)
+        self.assertEqual(len(all_rows), N_FILES, 'number of rows')
+        self.assertEqual(all_rows, EXPECTED, 'raw values')
+
+    def test_get_top_rows_invalid_class_balance2(self):
+        N_ROWS = 3
+        N_FILES = 3
+        EXPECTED = [[['st1840.png', 'knot', '0.12036637', '0.18497443', '0.7618415', '0.8283344', '512', '488', 'board_images_png', '0.986', '0.986'],
+                     ['st1840.png', 'knot', '0.7297609', '0.7755673', '0.62443626', '0.6670296', '512', '488', 'board_images_png', '0.986', '0.986'],
+                     ['st1840.png', 'defect', '0.76513', '0.9952971', '0.6075407', '0.6546806', '512', '488', 'board_images_png', '0.986', '0.986']],
+                     [['st1578.png', 'knot', '0.594302', '0.6663906', '0.35276932', '0.43525606', '512', '488', 'board_images_png', '0.98448783', '0.98448783']],
+                     [['st1611.png', 'knot', '0.6326234', '0.7054164', '0.86741334', '0.96444726', '512', '488', 'board_images_png', '0.99616516', '0.9843567'],
+                      ['st1611.png', 'knot', '0.07399843', '0.11282173', '0.32572043', '0.36819047', '512', '488', 'board_images_png', '0.9843567', '0.9843567']]]
+
+        class_balance = '0.1, 0.2, 0.3'
+        all_rows = get_top_rows(self.csv_file_loc, N_ROWS, self.user_folders ,
+                         self.pick_max, self.tag_names, class_balance)
+        self.assertEqual(len(all_rows), N_FILES, 'number of rows')
+        self.assertEqual(all_rows, EXPECTED, 'raw values')
+
+    def test_get_top_rows_class_balance_min(self):
+        N_ROWS = 3
+        N_FILES = 3
+        EXPECTED = [[['st1091.png', 'knot', '0.20989896', '0.251748', '0.34986168', '0.3921352', '512', '488', 'board_images_png', '0.99201256', '0.70161'],
+                                     ['st1091.png', 'knot', '0.696119', '0.7461088', '0.27078417', '0.33086362', '512', '488', 'board_images_png', '0.9827361', '0.70161'],
+                                     ['st1091.png', 'knot', '0.89531857', '0.93743694', '0.4605299', '0.5066802', '512', '488', 'board_images_png', '0.9794672', '0.70161'],
+                                     ['st1091.png', 'defect', '0.7629506', '1.0', '0.6205898', '0.67307687', '512', '488', 'board_images_png', '0.74762243', '0.70161'],
+                                     ['st1091.png', 'knot', '0.14214082', '0.247842', '0.7355515', '0.8967391', '512', '488', 'board_images_png', '0.7072498', '0.70161'],
+                                     ['st1091.png', 'defect', '0.0', '0.1281265', '0.55038965', '0.59755194', '512', '488', 'board_images_png', '0.70161', '0.70161']],
+                                    [['st1185.png', 'knot', '0.6978268', '0.7582275', '0.66821593', '0.7535644', '512', '488', 'board_images_png', '0.97257924', '0.7035888'],
+                                     ['st1185.png', 'defect', '0.35780182', '0.60781866', '0.27580062', '0.32093963', '512', '488', 'board_images_png', '0.9720861', '0.7035888'],
+                                     ['st1185.png', 'knot', '0.5183983', '0.57071316', '0.84764653', '0.91617334', '512', '488', 'board_images_png', '0.9241496', '0.7035888'],
+                                     ['st1185.png', 'knot', '0.55567926', '0.5904746', '0.51832056', '0.5461106', '512', '488', 'board_images_png', '0.7035888', '0.7035888']],
+                                    [['st1192.png', 'knot', '0.39846605', '0.45543727', '0.36765742', '0.4488806', '512', '488', 'board_images_png', '0.99612194', '0.7127546'],
+                                     ['st1192.png', 'defect', '0.07790943', '0.44866413', '0.5975798', '0.640683', '512', '488', 'board_images_png', '0.80447847', '0.7127546'],
+                                     ['st1192.png', 'defect', '0.47953823', '0.7499259', '0.5517361', '0.59940904', '512', '488', 'board_images_png', '0.7127546', '0.7127546']]]
+
+        pick_max = False
+        all_rows = get_top_rows(self.csv_file_loc, N_ROWS, self.user_folders,
+                                pick_max, self.tag_names, self.ideal_class_balance)
+        self.assertEqual(len(all_rows), N_FILES, 'number of rows')
+        self.assertEqual(all_rows, EXPECTED, 'raw values')
+
+    #@unittest.skip
     def test_create_vott_json(self):
         N_ROWS = 3
         N_FILES = 3
@@ -62,7 +133,7 @@ class DownloadVOTTJSONTestCase(unittest.TestCase):
                          self.tagging_location, blob_credentials=None,
                          tag_names= self.tag_names,
                          max_tags_per_pixel=self. max_tags_per_pixel ,
-                         ideal_class_balance=self.ideal_class_balance)
+                         config_class_balance=self.ideal_class_balance)
 
         res_folder = os.path.join(self.tagging_location, FOLDER_NAME)
         res_immages_cnt = sum([len(files) for r, d, files in os.walk(res_folder)])
