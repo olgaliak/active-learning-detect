3277a4fc098b76ad4a6b26394b9abcda364483af
Andre <me@andrebriggs.com>
2018-10-25 14:10:53 -0700

    Updated seed data. Fixed trigger behavior

--- db/SeedData.sql
+++ db/SeedData.sql
@@ -5,10 +5,12 @@ INSERT INTO TagState VALUES (2, 'Tagged')
 INSERT INTO TagState VALUES (3, 'Abandoned')
 
 -- Create fake image entries
-INSERT INTO Image_Info (ImageName,Folder,Height,Width) VALUES ('MyTestImage.jpg', 'SomeFolder', 40,40)
-INSERT INTO Image_Info (ImageName,Folder,Height,Width) VALUES ('AnotherImage.jpg', 'Folder2', 60, 80)
+INSERT INTO Image_Info (OriginalImageName,ImageLocation,Height,Width) 
+VALUES ('MyTestImage.jpg', 'https://csehackstorage.blob.core.windows.net/image-to-tag/1.jpg', 40,40)
+INSERT INTO Image_Info (OriginalImageName,ImageLocation,Height,Width) 
+VALUES ('AnotherImage.jpg', 'https://csehackstorage.blob.core.windows.net/image-to-tag/2.jpg', 60, 80)
 
 -- Create untagged states for the 2 fake images. Assumes the Image Ids are 1 and 2
-INSERT INTO Image_Tagging (ImageId,TagStateId) VALUES (1, 0)
-INSERT INTO Image_Tagging (ImageId,TagStateId) VALUES (2, 0)
+INSERT INTO Image_Tagging_State (ImageId,TagStateId) VALUES (1, 0)
+INSERT INTO Image_Tagging_State (ImageId,TagStateId) VALUES (2, 0)
 
--- db/TableSchemasV2.sql
+++ db/TableSchemasV2.sql
@@ -69,7 +69,7 @@ CREATE OR REPLACE FUNCTION log_image_tagging_state_changes()
         BEGIN
             IF NEW.TagStateId <> OLD.TagStateId THEN
                 INSERT INTO Image_Tagging_State_Audit(ImageId,TagStateId,ModifiedDtim,ArchiveDtim,ActionFlag)
-                VALUES(OLD.ImageId,OLD.TagStateId,OLD.ModifiedDtim,current_timestamp,2);
+                VALUES(NEW.ImageId,NEW.TagStateId,NEW.ModifiedDtim,current_timestamp,2);
             END IF;
             
             RETURN NEW;
