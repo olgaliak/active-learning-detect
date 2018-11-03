810538d6bb9a975c924af0509aeacfadc17c76d6
Michael Tarng <20913254+mtarng@users.noreply.github.com>
2018-11-02 19:00:08 -0700

    Vott Download Json

--- functions/pipeline/download/__init__.py
+++ functions/pipeline/download/__init__.py
@@ -3,7 +3,9 @@ import logging
 import azure.functions as func
 import json
 import os
+
 from ..shared import db_access as DB_Access
+from ..shared import vott_json_parser as vott_json_parser
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -22,11 +24,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     else:
         # setup response object
         connection = DB_Access.get_connection()
-        images = DB_Access.get_images_for_tagging(connection, imageCount)
+        # TODO: images need more meaningful data than just download urls
+        image_urls = DB_Access.get_images_for_tagging(connection, imageCount)
 
         # TODO: Build vott json
+        vott_json = vott_json_parser.create_starting_json(image_urls)
 
-        content = json.dumps(images)
+        return_body_json = {"imageUrls": image_urls, "vottJson": vott_json}
+
+        content = json.dumps(return_body_json)
         return func.HttpResponse(
             status_code=200, 
             headers=headers, 
--- functions/pipeline/shared/vott_json_parser.py
+++ functions/pipeline/shared/vott_json_parser.py
@@ -1,9 +1,25 @@
 import json
 
-def __get_id_from_filename(filename):
+def __build_frames_data(images):
+    frames = {}
+    for filename in images:
+        # TODO: Build tag data per frame if they exist already
+        frames[__get_filename_from_fullpath(filename)] = [] #list of tags
+    return frames
+
+# TODO: Change return from db to have more tag data...for now input is a list of blobstore urls
+def create_starting_json(images):
+    return {
+        "frames" : __build_frames_data(images),
+        "inputTags": "", # TODO: populate classifications that exist in db already
+    }
+
+def __get_filename_from_fullpath(filename):
     path_components = filename.split('/')
-    filename = path_components[-1]
-    return int(filename.split('.')[0])
+    return path_components[-1]
+
+def __get_id_from_fullpath(fullpath):
+    return int(__get_filename_from_fullpath(fullpath).split('.')[0])
 
 def process_vott_json(json):
     all_frame_data = json['frames']
@@ -11,13 +27,13 @@ def process_vott_json(json):
     # Scrub filename keys to only have integer Id, drop path and file extensions.
     id_to_tags_dict = {}
     for full_path_key in sorted(all_frame_data.keys()):
-        id_to_tags_dict[__get_id_from_filename(full_path_key)] = all_frame_data[full_path_key]
+        id_to_tags_dict[__get_id_from_fullpath(full_path_key)] = all_frame_data[full_path_key]
     all_ids = id_to_tags_dict.keys()
 
     # Do the same with visitedFrames
     visited_ids = sorted(json['visitedFrames'])
     for index, filename in enumerate(visited_ids):
-        visited_ids[index] = __get_id_from_filename(filename)
+        visited_ids[index] = __get_id_from_fullpath(filename)
 
     # Unvisisted imageIds
     unvisited_ids = sorted(list(set(all_ids) - set(visited_ids)))
@@ -29,3 +45,17 @@ def process_vott_json(json):
             "imagesVisited" : visited_ids,
             "imagesNotVisited" : unvisited_ids
         }
+
+def main():
+    images = {
+		"1012.png" : {},
+		"1013.png" : {},
+		"1014.png" : {},
+		"1015.png" : {},
+		"1016.png" : {}
+	}
+    generated_json = create_starting_json(images)
+    print(json.dumps(generated_json))
+
+if __name__ == '__main__':
+    main()
