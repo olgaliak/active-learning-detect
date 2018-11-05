ac341f8c80967504d91b400943cc860a6ece62e7
Samiya <samiyaakhtar7@gmail.com>
2018-11-05 15:37:36 -0800

    jupyter notebook for visualization dashboard first pass (#20)

--- dashboard/static/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-
-import { ActiveLearningStatistics } from './activelearningstatistics';
-
-    ReactDOM.render(
-        <ActiveLearningStatistics/>,
-        document.getElementById('root')
-    );
